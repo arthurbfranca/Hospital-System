66a90a3f7068d0eb2d14c20f0e48818844ed1e70
sydneykwok <47401206+sydneykwok@users.noreply.github.com>
2020-03-03 11:36:09 -0700

    Files for basic web app. Only login is "working" right now.

---
+++ HospitalManagementSystem/src/com/hospitalmanagement/LoginServlet.java
@@ -0,0 +1,49 @@
+package com.hospitalmanagement;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//doGet(request, response);
+		// accept request from user
+				String user = request.getParameter("user");
+				String pass = request.getParameter("pass");
+				
+				// complete some task
+				PrintWriter out = response.getWriter();
+				out.println("Succesfully logged in. Welcome, " + user +"!");
+	}
+
+}
