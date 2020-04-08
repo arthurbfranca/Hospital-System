fe3984eb30996912d57ed4883d18e0f0f0e52212
shavonnetran <47336813+shavonnetran@users.noreply.github.com>
2020-04-07 23:06:23 -0600

    added bg color

--- src/hospitalmanagement/PatientTestResultView.java
+++ src/hospitalmanagement/PatientTestResultView.java
@@ -31,6 +31,7 @@ public class PatientTestResultView extends JFrame {
 		
 		// create panel for the frame
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(135, 206, 235));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
