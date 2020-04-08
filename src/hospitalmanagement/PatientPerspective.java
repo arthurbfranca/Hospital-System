1d0f2c4e8a55a45786744ae29ce41349a92c2528
shavonnetran <47336813+shavonnetran@users.noreply.github.com>
2020-04-07 23:17:38 -0600

    added bg color

--- src/hospitalmanagement/PatientPerspective.java
+++ src/hospitalmanagement/PatientPerspective.java
@@ -2,6 +2,7 @@ package hospitalmanagement;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Color;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
