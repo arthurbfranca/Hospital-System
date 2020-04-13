fa93a0ba8e18ed0b6651d97f362736119b1869b1
sydneykwok <47401206+sydneykwok@users.noreply.github.com>
2020-04-13 17:09:57 -0600

    appointment booking up and running for both patients and doctors

--- src/hospitalmanagement/DoctorBookAppointment.java
+++ src/hospitalmanagement/DoctorBookAppointment.java
@@ -1,8 +1,6 @@
 package hospitalmanagement;
 
 import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
@@ -10,16 +8,12 @@ import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.border.EmptyBorder;
 
 import com.github.cliftonlabs.json_simple.JsonArray;
